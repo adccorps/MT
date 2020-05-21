b3c62039367692859d3097bbcd7771facfd38807
oopsWJ <oopswj@163.com>
2020-05-21 16:57:39 +0800

    ----test 测试阶段未正式开发--- redis服务上线

--- mt-commons/src/main/java/com/mt/pojo/User.java
+++ mt-commons/src/main/java/com/mt/pojo/User.java
@@ -1,5 +1,41 @@
 package com.mt.pojo;
 
 public class User {
+    public String id;
+    public String name;
+    public String password;
+    public String database;
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
--- mt-redis/src/main/java/com/mt/redis/config/RedisConfig.java
+++ mt-redis/src/main/java/com/mt/redis/config/RedisConfig.java
@@ -3,12 +3,15 @@ package com.mt.redis.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -18,7 +21,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableCaching
-@RefreshScope //自动刷新配置
+//@RefreshScope //自动刷新配置
 public class RedisConfig  extends CachingConfigurerSupport {
 
     @Value("${spring.redis.host}")
@@ -41,6 +44,7 @@ public class RedisConfig  extends CachingConfigurerSupport {
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
         // Json序列化配置
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
@@ -63,7 +67,8 @@ public class RedisConfig  extends CachingConfigurerSupport {
         return template;
     }
 
-    @Bean
+
+  /*  @Bean
     public JedisConnectionFactory redisConnectionFactory() {
         JedisConnectionFactory factory = new JedisConnectionFactory();
         factory.setHostName(host);
@@ -75,5 +80,5 @@ public class RedisConfig  extends CachingConfigurerSupport {
         factory.getPoolConfig().setMaxTotal(maxActive);
         factory.getPoolConfig().setMaxWaitMillis(maxWait);
         return factory;
-    }
+    }*/
 }
