9a99e31ff07b1d3dacec5cd1e40bb0ed708d6821
oopsWJ <oopswj@163.com>
2020-06-03 21:39:52 +0800

    评论模块更新

--- mt-comment/src/main/java/com/mt/comment/commentApplication.java
+++ mt-comment/src/main/java/com/mt/comment/commentApplication.java
@@ -1,4 +1,17 @@
 package com.mt.comment;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableEurekaClient //在服务启动后自动注册到Eureka中！
+@EnableDiscoveryClient
+@ComponentScan("com.mt")
 public class commentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(commentApplication.class, args);
+    }
 }
