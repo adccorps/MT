c92b69e19794fd375534ea99931aa4fc6af0cfda
test <2576109500@qq.com>
2020-06-06 19:05:16 +0800

    更改验证码信息保存到数据库的方式

--- mt-messages/src/main/java/com/mt/service/impl/MessageServiceImpl.java
+++ mt-messages/src/main/java/com/mt/service/impl/MessageServiceImpl.java
@@ -2,13 +2,14 @@ package com.mt.service.impl;
 
 import com.mt.config.Encryption;
 import com.mt.pojo.Messages;
-import com.mt.redis.RedisUtils;
+//import com.mt.redis.RedisUtils;
 import com.mt.service.MessageService;
 import com.mt.service.SMSUtil;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisConnectionUtils;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -19,9 +20,8 @@ public class MessageServiceImpl implements MessageService {
     SMSUtil sMSUtil;
     @Autowired
     Encryption encryption;
-
     @Autowired
-    RedisUtils redisUtils;
+    private StringRedisTemplate redisTemplate;
 //    RedisTemplate<Object, Messages> messageRedisTemplate;
 
     @RabbitListener(queues = "message.register")
@@ -33,10 +33,10 @@ public class MessageServiceImpl implements MessageService {
         //把type、用户手机号码、经过加密的code加入到实体类message中
         //Messages message = new Messages(type, phone, enCode.toString());
         //把message实体类加入到redis中，key值为用户的手机号码,并且有效期为5分钟
-        redisUtils.hset(phone,"type",type);
-        redisUtils.hset(phone,"phone",phone);
-        redisUtils.hset(phone,"code",enCode);
-        redisUtils.hsetTimeOut(phone,  5L,TimeUnit.MINUTES);
+        redisTemplate.opsForHash().put(phone, "type", type);
+        redisTemplate.opsForHash().put(phone, "phone", phone);
+        redisTemplate.opsForHash().put(phone, "code", enCode);
+        redisTemplate.expire(phone, 5, TimeUnit.MINUTES);
 //        messageRedisTemplate.opsForValue().set(message.getPhone(), message, 5, TimeUnit.MINUTES);
     }
 
@@ -49,10 +49,10 @@ public class MessageServiceImpl implements MessageService {
         //把type、用户手机号码、经过加密的code加入到实体类message中
         //Messages message = new Messages(type,phone,enCode.toString());
         //把message实体类加入到redis中，key值为用户的手机号码,并且有效期为5分钟
-        redisUtils.hset(phone,"type",type);
-        redisUtils.hset(phone,"phone",phone);
-        redisUtils.hset(phone,"code",enCode);
-        redisUtils.hsetTimeOut(phone,  5L,TimeUnit.MINUTES);
+        redisTemplate.opsForHash().put(phone, "type", type);
+        redisTemplate.opsForHash().put(phone, "phone", phone);
+        redisTemplate.opsForHash().put(phone, "code", enCode);
+        redisTemplate.expire(phone, 5, TimeUnit.MINUTES);
 //        messageRedisTemplate.opsForValue().set(message.getPhone(),message,5, TimeUnit.MINUTES);
     }
 
