ea5d4b4911fcb5bdcd2c6fb0155e2c052bad1c4e
test <2576109500@qq.com>
2020-06-26 10:03:36 +0800

    更改redis的code为enCode,添加swagger

--- mt-messages/src/main/java/com/mt/controller/MessagesController.java
+++ mt-messages/src/main/java/com/mt/controller/MessagesController.java
@@ -3,11 +3,16 @@ package com.mt.controller;
 import com.mt.constants.Code;
 import com.mt.pojo.Result;
 import com.mt.service.VeificationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(value = "验证码验证接口", tags = { "电影访问接口" })
 @RestController
 public class MessagesController {
     @Autowired
@@ -30,10 +35,18 @@ public class MessagesController {
      * @param phone
      * @return
      */
+    @ApiOperation(value = "登录验证，根据手机号码发送验证码")
     @RequestMapping("/login/message")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "发送成功"),
+    @ApiResponse(code = 500, message = "发送失败")})
     public Result requestLoginCode(String phone){
-        veificationService.sendLoginCode(phone);
-        Result MessagesResult = new Result(Code.OK,null);
+        Result MessagesResult;
+        if(phone == null){
+            MessagesResult = new Result(Code.INTERNAL_SERVER_ERROR,null);
+        }else{
+            veificationService.sendLoginCode(phone);
+            MessagesResult = new Result(Code.OK,null);
+        }
         return MessagesResult;
     }
 }
